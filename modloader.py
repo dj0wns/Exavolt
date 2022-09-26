b8e6547b3376d1ce2974b41eec29e9ea9cfdc4d0
Derek Jones <derekjones@asu.edu>
2022-09-26 00:03:11 -0400

    Added sensible default arguments

--- modloader.py
+++ modloader.py
@@ -36,9 +36,9 @@ def execute(input_iso, output_iso, mod_folder):
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Add mods to Metal Arms ISO file")
-  parser.add_argument("input_iso", help="A valid vanilla Metal Arms Iso File")
-  parser.add_argument("output_iso", help="Name of the new output iso which will be produced")
-  parser.add_argument("mod_folder", help="Folder containing all mods which the user will have the option of adding")
+  parser.add_argument("input_iso", help="A valid vanilla Metal Arms Iso File", nargs='?', default='metalarms.iso')
+  parser.add_argument("output_iso", help="Name of the new output iso which will be produced", nargs='?', default='mod.iso')
+  parser.add_argument("mod_folder", help="Folder containing all mods which the user will have the option of adding", nargs='?', default='mods')
   args = parser.parse_args()
 
   execute(args.input_iso, args.output_iso, args.mod_folder)
