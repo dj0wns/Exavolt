af4416b32d62ceb4639c4175207d4a3c6ba04fbc
Derek Jones <derekjones@asu.edu>
2023-07-12 16:39:04 -0400

    Some fixes for something broken in pyiso tools and windows

--- lib/iso.py
+++ lib/iso.py
@@ -15,6 +15,8 @@ def extract_iso(input_iso, out_dir = ""):
   print(f'Extracting {input_iso} to {out_dir}')
   src = Path(input_iso).resolve()
   iso = GamecubeISO.from_iso(src)
+  # bug in pyisotools
+  iso._onPhysicalTaskStart = None
   iso.extract(out_dir, dumpPositions=False)
   if debug:
     return out_dir
@@ -27,4 +29,6 @@ def rebuild_iso(output_iso, iso_dir):
   print(f'Rebuilding {iso_dir} to {output_iso}')
   src = Path(iso_dir).resolve()
   iso = GamecubeISO.from_root(src)
+  # bug in pyisotools
+  iso._onVirtualTaskStart = None
   iso.build(output_iso, preCalc = False)
