17620af1ad9bfe490ad1e98820ee337cf0cef8a2
Derek Jones <princeyury@gmail.com>
2025-10-14 09:51:57 -1000

    basic framework

---
+++ lib/csv_table_reader.py
@@ -0,0 +1,26 @@
+import csvreader
+
+# Description dict is a dict of table names and field counts per row
+# EX: (pick_level$.csv)
+# description_dict = {
+#     'pick_level_text' : 7,
+#     'pick_level_meshes' : 5,
+#     'pick_level_buttons' : 6,
+#     'level_names' : 6,
+#     'level_unlocking' : 1
+# }
+#
+#
+# Will return a dict of the following format:
+# return_dict = {
+#     'pick_level_text' : [[...]],
+#     'pick_level_meshes' : [[...]],
+#     'pick_level_buttons' : [[...]],
+#     'level_names' : [[...]],
+#     'level_unlocking' : [[...]]
+# }
+
+
+def read_ma_csv_to_dict(file, description_dict):
+  #TODO
+  pass
